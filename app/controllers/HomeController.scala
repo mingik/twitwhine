d2a6109ae7b4e8c6078e24aa8a67555eed144cbe
yank <mingiyan@gmail.com>
2016-04-04 17:41:17 -0700

    added twitter stream route

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,25 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.scaladsl._
+import akka.util.ByteString
+import play.api.http.Status
+import play.api.libs.iteratee.Iteratee
+import play.api.{Configuration, Logger}
+
+import scala.concurrent.{ExecutionContext, Future}
 import play.api.mvc._
+import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
+import play.api.libs.ws.{StreamedResponse, WSClient}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(configuration: Configuration, ws: WSClient)(implicit val mat: Materializer, exec: ExecutionContext) extends Controller {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -21,4 +31,32 @@ class HomeController @Inject() extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def tweets = Action.async {
+    val credentials: Option[(ConsumerKey, RequestToken)] = tweeterCredentials()
+
+    credentials.map { case (consumerKey, requestToken) =>
+      ws
+        .url("https://stream.twitter.com/1.1/statuses/filter.json")
+        .sign(OAuthCalculator(consumerKey, requestToken))
+        .withQueryString("track" -> "Twitter")
+        .withMethod("POST").stream()
+        .map(loggingStreamConsumer(_))
+        .map(_ => Ok("Stream closed"))
+    } getOrElse {
+      Future { InternalServerError("Twitter credentials missing") }
+    }
+  }
+
+  private def loggingStreamConsumer(streamedResponse: StreamedResponse) = {
+    Logger.info("Status: " + streamedResponse.headers.status)
+    streamedResponse.body.runForeach(byteString => Logger.info(byteString.utf8String))
+  }
+
+
+  private def tweeterCredentials(): Option[(ConsumerKey, RequestToken)] = for {
+    apiKey <- configuration.getString("twitter.apiKey")
+    apiSecret <- configuration.getString("twitter.apiSecret")
+    token <- configuration.getString("twitter.token")
+    tokenSecret <- configuration.getString("twitter.tokenSecret")
+  } yield (ConsumerKey(apiKey, apiSecret), RequestToken(token, tokenSecret))
 }
