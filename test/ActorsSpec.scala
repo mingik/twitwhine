99c30333270c3ce5cac0418808a273adb38cf59f
mingik <mingiyan@gmail.com>
2016-04-11 22:28:05 -0700

    added actor test

--- test/ActorsSpec.scala
+++ test/ActorsSpec.scala
@@ -1,25 +1,75 @@
 import actors.TwitterStreamer
 import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
 import akka.testkit.{TestActorRef, TestProbe, TestKit, ImplicitSender}
+import akka.util.ByteString
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike, Matchers}
 import play.api.Configuration
-import play.api.libs.ws.WSClient
+import play.api.libs.concurrent.Promise
+import play.api.libs.oauth.{RequestToken, ConsumerKey, OAuthCalculator}
+import play.api.libs.ws.{WSResponseHeaders, StreamedResponse, WSRequest, WSClient}
+
+import scala.concurrent.Future
 
 /**
  * Created by mintik on 4/10/16.
  */
-class ActorsSpec extends TestKit(ActorSystem("ActorsSpec")) with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll with MockFactory  {
+class ActorsSpec extends TestKit(ActorSystem("ActorsSpec")) with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll with MockFactory {
 
   "TweeterStreaamer" should {
     "reply with credentials missing message if no configuration was provided" in {
       val mockConfiguration = Configuration.empty
-      val wsClient = mock[WSClient]
+      val wsClient = stub[WSClient]
       val probe = TestProbe()
       val twitterStreamerActor = TestActorRef(new TwitterStreamer(mockConfiguration, wsClient, probe.ref))
       twitterStreamerActor ! "subscribe"
       twitterStreamerActor ! "twitter"
       probe.expectMsg("Twitter credentials missing")
+    }
+
+    "reply with 'Stream is closed' message if configuration was valid but source is not a valid JSON" in {
+      import scala.concurrent.ExecutionContext.Implicits.global
+      val validConfiguration = Configuration(
+        ("twitter.apiKey","apikey"),("twitter.apiSecret","apisecret"),
+        ("twitter.token","token"), ("twitter.tokenSecret","secret"))
+      val wsClient = stub[WSClient]
+      val wsRequest = stub[WSRequest]
+      (wsClient.url _).when("https://stream.twitter.com/1.1/statuses/filter.json").returns(wsRequest)
+      (wsRequest.sign _).when(*).returns(wsRequest)
+      (wsRequest.withQueryString _).when(List(("track", "twitter")).toSeq).returns(wsRequest)
+      (wsRequest.withMethod _).when("POST").returns(wsRequest)
+      val headers = stub[WSResponseHeaders]
+      val streamedResponse = Future { StreamedResponse(headers, Source.single(ByteString("Hello World"))) }
+      (wsRequest.stream _).when().returns(streamedResponse)
+      val probe = TestProbe()
+      val twitterStreamerActor = TestActorRef(new TwitterStreamer(validConfiguration, wsClient, probe.ref))
+      twitterStreamerActor ! "subscribe"
+      twitterStreamerActor ! "twitter"
+
+      probe.expectMsg("Stream is closed")
+    }
+
+    "reply with correct messages if configuration was valid and source is valid JSON" in {
+      import scala.concurrent.ExecutionContext.Implicits.global
+      val validConfiguration = Configuration(
+        ("twitter.apiKey","apikey"),("twitter.apiSecret","apisecret"),
+        ("twitter.token","token"), ("twitter.tokenSecret","secret"))
+      val wsClient = stub[WSClient]
+      val wsRequest = stub[WSRequest]
+      (wsClient.url _).when("https://stream.twitter.com/1.1/statuses/filter.json").returns(wsRequest)
+      (wsRequest.sign _).when(*).returns(wsRequest)
+      (wsRequest.withQueryString _).when(List(("track", "twitter")).toSeq).returns(wsRequest)
+      (wsRequest.withMethod _).when("POST").returns(wsRequest)
+      val headers = stub[WSResponseHeaders]
+      val streamedResponse = Future { StreamedResponse(headers, Source.single(ByteString("{\"hello\": 33}"))) }
+      (wsRequest.stream _).when().returns(streamedResponse)
+      val probe = TestProbe()
+      val twitterStreamerActor = TestActorRef(new TwitterStreamer(validConfiguration, wsClient, probe.ref))
+      twitterStreamerActor ! "subscribe"
+      twitterStreamerActor ! "twitter"
+
+      probe.expectMsg("{\"hello\": 33}")
     }
   }
 }
